ae75f92cdb34ce46ad3e8defcbc31c5f62f23fd7
palindaa <palinda@xeptagon.com>
2022-03-31 15:54:23 +0530

    Add github actions

--- main/__init__.py
+++ main/__init__.py
@@ -17,7 +17,7 @@ app.include_router(imputation.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello SIDS ML Backend V2!"}
+    return {"message": "Hello SIDS ML Backend V6!"}
 
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
