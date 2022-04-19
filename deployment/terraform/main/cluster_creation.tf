a87f5efab4eabdfad962833be0d4eade91cc96ba
palindaa <palinda@xeptagon.com>
2022-04-19 18:21:29 +0530

    added time log

--- deployment/terraform/main/cluster_creation.tf
+++ deployment/terraform/main/cluster_creation.tf
@@ -60,3 +60,13 @@ resource "azurerm_kubernetes_cluster" "clusterAppGW" {
   }
 }
 
+#resource "azurerm_kubernetes_cluster_node_pool" "survey" {
+#  name                  = "internal"
+#  kubernetes_cluster_id = azurerm_kubernetes_cluster.clusterNGINX[0].id
+#  vm_size               = "Standard_DS2_v2"
+#  node_count            = 1
+#
+#  tags = {
+#    Environment = "Testing"
+#  }
+#}
